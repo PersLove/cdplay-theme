de961d9da1b2ac1836acc364167a63cd9b9771e0
Сергей Persey <perslove@gmail.com>
2026-05-19 11:40:40 +0300

    Use filemtime for asset cache busting

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -13,18 +13,24 @@ if (!defined('ABSPATH')) {
  * Enqueue frontend styles and scripts.
  */
 function cdplay_enqueue_assets() {
+	$cdplay_theme_version = wp_get_theme()->get('Version');
+	$cdplay_main_css_path = CDPLAY_THEME_DIR . '/assets/css/main.css';
+	$cdplay_main_js_path  = CDPLAY_THEME_DIR . '/assets/js/main.js';
+	$cdplay_main_css_ver  = file_exists($cdplay_main_css_path) ? filemtime($cdplay_main_css_path) : $cdplay_theme_version;
+	$cdplay_main_js_ver   = file_exists($cdplay_main_js_path) ? filemtime($cdplay_main_js_path) : $cdplay_theme_version;
+
 	wp_enqueue_style(
 		'cdplay-main',
 		CDPLAY_THEME_URI . '/assets/css/main.css',
 		array(),
-		CDPLAY_VERSION
+		$cdplay_main_css_ver
 	);
 
 	wp_enqueue_script(
 		'cdplay-main',
 		CDPLAY_THEME_URI . '/assets/js/main.js',
 		array(),
-		CDPLAY_VERSION,
+		$cdplay_main_js_ver,
 		true
 	);
 }
